8a246090fd87f2a0abe4c7d97f98901a446becdc
vulnerabbity <you@example.com>
2022-07-14 05:05:01 +0700

    add search results models

---
+++ src/models/SearchResult.ts
@@ -0,0 +1,30 @@
+import { Channel } from "./Channel"
+import { MusicType } from "./Music"
+import { Playlist } from "./Playlist"
+import { Video } from "./Video"
+
+export type SearchItem = Video | Channel | Playlist | MusicType
+
+export interface SearchResult<T extends SearchItem = SearchItem> {
+  items: T[]
+
+  /**
+   * Encoded string to continue searching
+   */
+  nextpage: string | null
+
+  /**
+   * If typed incorrectly will be correction string
+   *
+   * Default is empty string
+   *
+   * Example:
+   * rick astleyy -> rick astley
+   */
+  suggestion: string
+
+  /**
+   * If typed incorrectly will be true
+   */
+  corrected: boolean
+}
