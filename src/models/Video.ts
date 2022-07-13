8067b6b5e01c6730261c3b2ae8cb3a0bb074804f
vulnerabbity <you@example.com>
2022-07-13 23:22:06 +0700

    add Video model

---
+++ src/models/Video.ts
@@ -0,0 +1,17 @@
+export interface Video {
+  duration: number;
+
+  thumbnail: string;
+
+  title: string;
+
+  uploadedDate: string;
+
+  uploaderAvatar: string;
+
+  uploaderUrl: string;
+
+  url: string;
+
+  views: number;
+}
