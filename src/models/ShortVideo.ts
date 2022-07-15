46ba9f4c6e1008106e63a99a27db9714d7384c92
vulnerabbity <you@example.com>
2022-07-16 01:46:40 +0700

    add short video model tests

--- src/models/ShortVideo.ts
+++ src/models/ShortVideo.ts
@@ -1,4 +1,6 @@
+import { TestUtils } from "../utils/TestUtils"
 import { Video } from "./Video"
+import { expect } from "chai"
 
 /**
  * Same shape with video with some fields hardcoded
@@ -10,4 +12,39 @@ export type ShortVideo = Omit<Video, "uploadedDate" | "uploaderAvatar" | "shortD
   readonly views: -1
   readonly uploaded: -1
   readonly uploaderVerified: boolean
+}
+
+export function testShortVideos(videos: ShortVideo[]) {
+  videos.forEach(video => testShortVideo(video))
+}
+
+export function testShortVideo(video: ShortVideo) {
+  const {
+    duration,
+    shortDescription,
+    thumbnail,
+    title,
+    uploaded,
+    uploadedDate,
+    uploaderAvatar,
+    uploaderName,
+    uploaderUrl,
+    uploaderVerified,
+    url,
+    views,
+  } = video
+
+  TestUtils.expectNumber(duration)
+  TestUtils.expectNull(shortDescription)
+  TestUtils.expectString(thumbnail)
+  TestUtils.expectString(title)
+  TestUtils.expectNull(uploadedDate)
+  TestUtils.expectNull(uploaderAvatar)
+  TestUtils.expectString(uploaderName)
+  TestUtils.expectString(uploaderUrl)
+  TestUtils.expectBoolean(uploaderVerified)
+  TestUtils.expectString(url)
+
+  expect(uploaded).equal(-1)
+  expect(views).equal(-1)
 }
