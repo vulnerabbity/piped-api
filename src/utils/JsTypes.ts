136b5159a5fb18342687044eabe8b559d3bb84a6
vulnerabbity <you@example.com>
2022-07-15 22:49:32 +0700

    add JSTypes class

---
+++ src/utils/JsTypes.ts
@@ -0,0 +1,10 @@
+class JSTypes {
+  readonly string = "string"
+  readonly number = "number"
+  readonly object = "object"
+  readonly boolean = "boolean"
+}
+
+const jsTypes = new JSTypes()
+
+export { jsTypes as JSTypes }
