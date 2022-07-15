40e4740ffb1293c9f100dbc0621d31b8f4df0625
vulnerabbity <you@example.com>
2022-07-15 22:50:37 +0700

    add trends fetcher tests

---
+++ src/api/TrendsFetcher.test.ts
@@ -0,0 +1,17 @@
+import { TrendsFetcher } from "./TrendsFetcher"
+import { testVideo } from "../models/ModelTester"
+
+describe(TrendsFetcher.name, () => {
+  const trendsFetcher = new TrendsFetcher()
+  it("should fetch correct shape", async () => {
+    const videos = await trendsFetcher.fetch()
+
+    if (videos === null) {
+      throw new Error(`${TrendsFetcher.name}.fetch returned null`)
+    }
+
+    for (let video of videos) {
+      testVideo(video)
+    }
+  })
+})
