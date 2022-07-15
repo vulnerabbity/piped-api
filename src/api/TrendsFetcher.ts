807ac3a32a701746b784e833ccb758fecc6971ce
vulnerabbity <you@example.com>
2022-07-15 21:18:13 +0700

    fixed wrong type for trends fetcher

--- src/api/TrendsFetcher.ts
+++ src/api/TrendsFetcher.ts
@@ -1,24 +1,24 @@
-import { Video } from "../models";
-import { PipedFetcher } from "./PipedFetcher";
+import { Video } from "../models"
+import { PipedFetcher } from "./PipedFetcher"
 
 export class TrendsFetcher extends PipedFetcher {
-  protected readonly trendsUrl = this.baseUrl + "/trending";
+  protected readonly trendsUrl = this.baseUrl + "/trending"
 
-  async fetch(): Promise<Video | null> {
+  async fetch(): Promise<Video[] | null> {
     try {
-      const response = await this.httpClient.get<Video>(this.trendsUrl, {
+      const response = await this.httpClient.get<Video[]>(this.trendsUrl, {
         params: this.getRequestParams(),
-      });
+      })
 
-      return response.data;
+      return response.data
     } catch {
-      return null;
+      return null
     }
   }
 
   protected getRequestParams() {
     return {
       region: this.region,
-    };
+    }
   }
 }
